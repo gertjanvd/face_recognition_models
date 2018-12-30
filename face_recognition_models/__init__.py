8e3ccdcdf24c172d6d83ac01b5186037d3c0efb5
Gertjan Van Droogenbroeck <gertjanvd@gmail.com>
2018-12-30 20:26:27 +0100

    models: remove 68 face landmarks predictor for 0.1.0-reduced version

--- face_recognition_models/__init__.py
+++ face_recognition_models/__init__.py
@@ -2,12 +2,12 @@
 
 __author__ = """Adam Geitgey"""
 __email__ = 'ageitgey@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.1.0-reduced'
 
 from pkg_resources import resource_filename
 
 def pose_predictor_model_location():
-    return resource_filename(__name__, "models/shape_predictor_68_face_landmarks.dat")
+    return None
 
 def pose_predictor_five_point_model_location():
     return resource_filename(__name__, "models/shape_predictor_5_face_landmarks.dat")
